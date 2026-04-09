bf55e5ccec7aca263e651522c9197ae3eac0fb9f
AGB <giray@batiturk.com>
2026-04-09 22:46:30 +0300

    Initial Commit

---
+++ ForceQuitX/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ForceQuitX
+//
+//  Created by Giray BATITURK on 9.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ForceQuitX/ForceQuitXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForceQuitXApp.swift
+//  ForceQuitX
+//
+//  Created by Giray BATITURK on 9.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct ForceQuitXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
